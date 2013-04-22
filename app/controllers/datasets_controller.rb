3a003cfa5be62f972aa47a1a71991aecbe010957
Ben Foxall <benfoxall@gmail.com>
2013-04-22 17:48:49 +0100

    added unsaving create method

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -2,4 +2,9 @@ class DatasetsController < ApplicationController
   def new
     @dataset = Dataset.new
   end
+
+  def create
+    @dataset = Dataset.new(params[:dataset])
+    render 'new'
+  end
 end
