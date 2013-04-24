2b2cf6c39fbba539d091d3a009751780f98a9419
Ben Foxall <benfoxall@gmail.com>
2013-04-24 14:30:25 +0100

    updating dataset -> datasets naming

--- test/functional/datasets_controller_test.rb
+++ test/functional/datasets_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class DatasetControllerTest < ActionController::TestCase
+class DatasetsControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
