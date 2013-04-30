5055942e404b27e392b7bd41bfd17a189c09c8a6
Ben Foxall <benfoxall@gmail.com>
2013-04-30 08:12:20 +0100

    maintain the locale param when visiting different urls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :set_locale
  
+  # pick the locale from ?locale=X in the url,  a prettier
+  # solution might be used down the line, maybe depending
+  # on the ui or user preferences maybe.
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
+
+  # maintain the locale param on any urls
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
+
 end
