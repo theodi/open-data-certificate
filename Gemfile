14621347c579fd07a255017a53240ea4fb6f7ac5
Ben Foxall <benfoxall@gmail.com>
2013-05-02 14:30:25 +0100

    keep sqlite for testing, require factory girl in test setup

--- Gemfile
+++ Gemfile
@@ -5,10 +5,9 @@ gem 'rails', '3.2.13'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-# gem 'sqlite3'
-#gem "mongoid", "~> 3.0.0"
-# gem 'mongo_mapper'
-gem 'bson_ext'
+gem 'sqlite3'
+# gem "mongoid", "~> 3.0.0"
+# gem 'bson_ext'
 gem 'pg'
 
 gem 'surveyor'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'factory_girl'
+require 'factories'
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
