d9195b864025774afccbf158bf73effde65df370
michael <pavling@gmail.com>
2013-05-07 16:31:12 +0100

    Comment about alternate method of determining whether requirements are met

--- app/models/question.rb
+++ app/models/question.rb
@@ -20,8 +20,11 @@ class Question < ActiveRecord::Base
 
   def requirement_met_by_responses?(responses)
     # could use thusly to get all the displayed requirements for a survey and whether they have been met by their corresponding answers:
-    # response_set.survey.sections.map(&:questions).flatten.select{|e|e.is_a_requirement? && e.triggered?}.map{|e|[e.requirement, e.requirement_met_by_responses?(rs.responses)]}
-    responses.map(&:question_id).include?(question_corresponding_to_requirement.id) #TODO: I would fairly comfortable anticipate that the iteration of responses will need to move (for efficiency) to the place where we're calling the 'requirement_met_by_responses?' method
+    #   `response_set.survey.sections.map(&:questions).flatten.select{|e|e.is_a_requirement? && e.triggered?}.map{|e|[e.requirement, e.requirement_met_by_responses?(rs.responses)]}`
+    # or if we want to determine the triggered on the associated question rather than the label...
+    #   `response_set.survey.sections.map(&:questions).flatten.select{|e|e.is_a_requirement? && e.question_corresponding_to_requirement.triggered?}.map{|e|[e.requirement, e.requirement_met_by_responses?(rs.responses)]}`
+
+  responses.map(&:question_id).include?(question_corresponding_to_requirement.id) #TODO: I would fairly comfortable anticipate that the iteration of responses will need to move (for efficiency) to the place where we're calling the 'requirement_met_by_responses?' method
   end
 
   def default_args
