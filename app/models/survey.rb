a222b63744881c911c5f7cb235742fe63785848c
michael <pavling@gmail.com>
2013-05-10 10:12:09 +0100

    Adding some debugging output to Survey to aid troubleshooting parsing problems

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,4 +2,33 @@ class Survey < ActiveRecord::Base
   include Surveyor::Models::SurveyMethods
 
   REQUIREMENT_LEVELS = %w(basic pilot standard exemplar)
+
+  def valid?(context = nil)
+    super(context)
+    unless errors.empty?
+
+      sections.each do |section|
+        unless section.errors.empty?
+          puts "section '#{section.title}' errors: #{section.errors.full_messages}"
+
+          questions.each do |question|
+            unless question.errors.empty?
+              puts "question '#{question.text}' errors: #{question.errors.full_messages}"
+
+              question.answers.each do |answer|
+                puts "answer '#{answer.text}' errors: #{answer.errors.full_messages}" unless answer.errors.empty?
+              end
+
+            end
+          end
+
+        end
+      end
+
+    end
+
+    return errors.empty?
+  end
+
+
 end
