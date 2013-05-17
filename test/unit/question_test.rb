a99f26ae2734fbe877d876409acd9a35ed091ce4
michael <pavling@gmail.com>
2013-05-17 18:31:02 +0100

    100% test coverage of Question model

--- test/unit/question_test.rb
+++ test/unit/question_test.rb
@@ -49,5 +49,114 @@ class QuestionTest < ActiveSupport::TestCase
     assert requirement.question_corresponding_to_requirement == the_right_question
   end
 
+  test "#answer_corresponding_to_requirement should return the right answer" do
+    requirement = FactoryGirl.create(:question, requirement: "the_right_level_#{rand(10)}", display_type: 'label')
 
+    rand(1..5).times { FactoryGirl.create(:answer, requirement: "wrong_level_#{rand(10)}", question: FactoryGirl.create(:question, survey_section: requirement.survey_section)) } #wrong questions
+    the_right_answer = FactoryGirl.create(:answer, requirement: requirement.requirement, question: FactoryGirl.create(:question, survey_section: requirement.survey_section))
+    rand(1..5).times { FactoryGirl.create(:answer, requirement: "wrong_level_#{rand(10)}", question: FactoryGirl.create(:question, survey_section: requirement.survey_section)) } #wrong questions
+
+    requirement.survey_section.survey.reload
+
+    assert requirement.answer_corresponding_to_requirement == the_right_answer
+  end
+
+  test "for string answers - #requirement_met_by_responses? returns false when requirement is not met by responses" do
+    requirement = FactoryGirl.create(:question, requirement: 'standard_1', display_type: 'label')
+    question = FactoryGirl.create(:question, survey_section: requirement.survey_section)
+    answer = FactoryGirl.create(:answer, requirement: requirement.requirement, question: question)
+    response_set = FactoryGirl.create(:response_set, survey: question.survey_section.survey)
+
+    requirement.survey_section.survey.reload
+    response_set.reload
+    assert_false requirement.requirement_met_by_responses?(response_set.responses)
+  end
+
+  test "for string answers - #requirement_met_by_responses? returns true when requirement is met by responses" do
+    requirement = FactoryGirl.create(:question, requirement: 'standard_1', display_type: 'label')
+    question = FactoryGirl.create(:question, survey_section: requirement.survey_section)
+    answer = FactoryGirl.create(:answer, requirement: requirement.requirement, question: question)
+    response_set = FactoryGirl.create(:response_set, survey: question.survey_section.survey)
+    FactoryGirl.create(:response, response_set: response_set, question_id: question.id, answer_id: answer.id, string_value: 'my answer')
+
+    requirement.survey_section.survey.reload
+    response_set.reload
+    assert requirement.requirement_met_by_responses?(response_set.responses)
+  end
+
+  test "for checkbox answers - #requirement_met_by_responses? returns false when requirement is not met by responses" do
+    requirement = FactoryGirl.create(:question, requirement: 'standard_1', display_type: 'label')
+    question = FactoryGirl.create(:question, survey_section: requirement.survey_section, pick: :any)
+    answer1 = FactoryGirl.create(:answer, requirement: 'basic_1', question: question)
+    answer2 = FactoryGirl.create(:answer, requirement: 'pilot_1', question: question)
+    answer3 = FactoryGirl.create(:answer, requirement: 'standard_1', question: question)
+    answer4 = FactoryGirl.create(:answer, requirement: 'exemplar_1', question: question)
+    response_set = FactoryGirl.create(:response_set, survey: question.survey_section.survey)
+    FactoryGirl.create(:response, response_set: response_set, question_id: question.id, answer_id: answer1.id)
+    FactoryGirl.create(:response, response_set: response_set, question_id: question.id, answer_id: answer2.id)
+
+    requirement.survey_section.survey.reload
+    response_set.reload
+    assert_false requirement.requirement_met_by_responses?(response_set.responses)
+  end
+
+  test "for checkbox answers - #requirement_met_by_responses? returns true when requirement is met by responses" do
+    requirement = FactoryGirl.create(:question, requirement: 'standard_1', display_type: 'label')
+    question = FactoryGirl.create(:question, survey_section: requirement.survey_section, pick: :any)
+    answer1 = FactoryGirl.create(:answer, requirement: 'basic_1', question: question)
+    answer2 = FactoryGirl.create(:answer, requirement: 'pilot_1', question: question)
+    answer3 = FactoryGirl.create(:answer, requirement: 'standard_1', question: question)
+    answer4 = FactoryGirl.create(:answer, requirement: 'exemplar_1', question: question)
+    response_set = FactoryGirl.create(:response_set, survey: question.survey_section.survey)
+    FactoryGirl.create(:response, response_set: response_set, question_id: question.id, answer_id: answer3.id)
+
+    requirement.survey_section.survey.reload
+    response_set.reload
+    assert requirement.requirement_met_by_responses?(response_set.responses)
+  end
+
+  test "for radio answers - #requirement_met_by_responses? returns false when requirement is not met by responses" do
+    requirement = FactoryGirl.create(:question, requirement: 'standard_1', display_type: 'label')
+    question = FactoryGirl.create(:question, survey_section: requirement.survey_section, pick: :one)
+    answer1 = FactoryGirl.create(:answer, requirement: 'basic_1', question: question)
+    answer2 = FactoryGirl.create(:answer, requirement: 'pilot_1', question: question)
+    answer3 = FactoryGirl.create(:answer, requirement: 'standard_1', question: question)
+    answer4 = FactoryGirl.create(:answer, requirement: 'exemplar_1', question: question)
+    response_set = FactoryGirl.create(:response_set, survey: question.survey_section.survey)
+    FactoryGirl.create(:response, response_set: response_set, question_id: question.id, answer_id: answer2.id)
+
+    requirement.survey_section.survey.reload
+    response_set.reload
+    assert_false requirement.requirement_met_by_responses?(response_set.responses)
+  end
+
+  test "for radio answers - #requirement_met_by_responses? returns false when requirement is met exactly by responses" do
+    requirement = FactoryGirl.create(:question, requirement: 'standard_1', display_type: 'label')
+    question = FactoryGirl.create(:question, survey_section: requirement.survey_section, pick: :one)
+    answer1 = FactoryGirl.create(:answer, requirement: 'basic_1', question: question)
+    answer2 = FactoryGirl.create(:answer, requirement: 'pilot_1', question: question)
+    answer3 = FactoryGirl.create(:answer, requirement: 'standard_1', question: question)
+    answer4 = FactoryGirl.create(:answer, requirement: 'exemplar_1', question: question)
+    response_set = FactoryGirl.create(:response_set, survey: question.survey_section.survey)
+    FactoryGirl.create(:response, response_set: response_set, question_id: question.id, answer_id: answer3.id)
+
+    requirement.survey_section.survey.reload
+    response_set.reload
+    assert requirement.requirement_met_by_responses?(response_set.responses)
+  end
+
+  test "for radio answers - #requirement_met_by_responses? returns false when requirement is exceeded by responses" do
+    requirement = FactoryGirl.create(:question, requirement: 'standard_1', display_type: 'label')
+    question = FactoryGirl.create(:question, survey_section: requirement.survey_section, pick: :one)
+    answer1 = FactoryGirl.create(:answer, requirement: 'basic_1', question: question)
+    answer2 = FactoryGirl.create(:answer, requirement: 'pilot_1', question: question)
+    answer3 = FactoryGirl.create(:answer, requirement: 'standard_1', question: question)
+    answer4 = FactoryGirl.create(:answer, requirement: 'exemplar_1', question: question)
+    response_set = FactoryGirl.create(:response_set, survey: question.survey_section.survey)
+    FactoryGirl.create(:response, response_set: response_set, question_id: question.id, answer_id: answer4.id)
+
+    requirement.survey_section.survey.reload
+    response_set.reload
+    assert requirement.requirement_met_by_responses?(response_set.responses)
+  end
 end
