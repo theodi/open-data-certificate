8722a8f4ed8d76c1e530b16de855dd65f6b0a114
Peter West <pete@whiteoctober.co.uk>
2013-05-17 16:06:12 +0100

    Added default value for dataset titles

---
+++ db/migrate/20130517144237_add_default_title_to_dataset.rb
@@ -0,0 +1,9 @@
+class AddDefaultTitleToDataset < ActiveRecord::Migration
+  def up
+    change_column :datasets, :title, :string, :default => 'Untitiled'
+  end
+
+  def down
+    change_column :datasets, :title, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130516084640) do
+ActiveRecord::Schema.define(:version => 20130517144237) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -52,14 +52,14 @@ ActiveRecord::Schema.define(:version => 20130516084640) do
   end
 
   create_table "datasets", :force => true do |t|
-    t.string   "title"
+    t.string   "title",             :default => "Untitiled"
     t.string   "documentation_url"
     t.string   "curating_org"
     t.string   "curator_url"
     t.string   "data_kind"
     t.integer  "user_id"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
   end
 
   create_table "dependencies", :force => true do |t|
