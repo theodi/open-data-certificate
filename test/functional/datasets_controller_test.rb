26d0f8d9fed9a66c24b55ceb0435116e806fbd62
michael <pavling@gmail.com>
2013-05-22 13:55:37 +0100

    Correcting dataset_controller_tests

--- test/functional/datasets_controller_test.rb
+++ test/functional/datasets_controller_test.rb
@@ -3,12 +3,19 @@ require 'test_helper'
 class DatasetsControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
-  test "should get new" do
+  test "not signed_in users should get redirected to login" do
+    get :new
+    assert_response :redirect
+  end
+
+  test "signed_in users should get new" do
+    sign_in FactoryGirl.create(:user)
     get :new
     assert_response :success
   end
 
-  test "should create dataset" do
+  test "signed_in users should create dataset" do
+    sign_in FactoryGirl.create(:user)
     assert_difference('Dataset.count') do
       post :create, dataset: {title: 'my dataset' }
     end
