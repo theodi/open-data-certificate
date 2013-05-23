f2abba81550a222845c0b72a13f3f5538292b87a
Peter West <pete@whiteoctober.co.uk>
2013-05-23 18:30:59 +0100

    Disabled new survey validation until jeni provides the updated survey

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -79,7 +79,8 @@ class Survey < ActiveRecord::Base
     requirements.each do |requirement|
       amount = only_questions.select { |q| q != requirement && q.requirement == requirement.requirement }.count + answers.select { |a| a.requirement == requirement.requirement}.count
       if amount == 0
-        errors.add(:base, "requirement '#{requirement.reference_identifier}' is not linked to a question or answer")
+        # TODO: Re-enable this when the survey is fixed
+        #errors.add(:base, "requirement '#{requirement.reference_identifier}' is not linked to a question or answer")
       elsif amount > 1
         errors.add(:base, "requirement '#{requirement.reference_identifier}' is linked more than one question or answer")
       end
