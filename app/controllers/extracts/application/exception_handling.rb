89250280a5f7de41f248ad0798de39d3f150de3d
Michael Pavling <pavling@gmail.com>
2013-05-23 21:00:14 +0100

    Removing deprecated InstanceMethods module from mixin

--- app/controllers/extracts/application/exception_handling.rb
+++ app/controllers/extracts/application/exception_handling.rb
@@ -13,42 +13,40 @@ module Extracts
         end
       end
 
-      module InstanceMethods
-        def routing_error
-          # For catching and re-raising routing errors in the application - see the comment and route appended in application.rb
-          raise ActionController::RoutingError.new("No route matches [#{request.method}] \"#{request.fullpath}\"")
+      def routing_error
+        # For catching and re-raising routing errors in the application - see the comment and route appended in application.rb
+        raise ActionController::RoutingError.new("No route matches [#{request.method}] \"#{request.fullpath}\"")
+      end
+
+      private
+      def render_not_found(exception)
+        log_error(exception)
+        render :template => "/errors/404", :status => 404
+      end
+
+      private
+      def render_error(exception)
+        log_error(exception)
+        render :template => "/errors/500", :status => 500
+      end
+
+      private
+      def log_error(exception)
+        # Mostly taken from actionpack-3.2.11/lib/action_dispatch/middleware/debug_exceptions.rb#log_error
+        # The "log_error" method in the exception handler is now private, and it was simpler just to paste the salient points here.
+
+        wrapper = ActionDispatch::ExceptionWrapper.new(request.env, exception)
+
+        trace = wrapper.application_trace
+        trace = wrapper.framework_trace if trace.empty?
+        ActiveSupport::Deprecation.silence do
+          message = "\n#{exception.class} (#{exception.message}):\n"
+          message << exception.annoted_source_code.to_s if exception.respond_to?(:annoted_source_code)
+          message << "  " << trace.join("\n  ")
+          logger.fatal("#{message}\n\n")
         end
 
-        private
-        def render_not_found(exception)
-          log_error(exception)
-          render :template => "/errors/404", :status => 404
-        end
-
-        private
-        def render_error(exception)
-          log_error(exception)
-          render :template => "/errors/500", :status => 500
-        end
-
-        private
-        def log_error(exception)
-          # Mostly taken from actionpack-3.2.11/lib/action_dispatch/middleware/debug_exceptions.rb#log_error
-          # The "log_error" method in the exception handler is now private, and it was simpler just to paste the salient points here.
-
-          wrapper = ActionDispatch::ExceptionWrapper.new(request.env, exception)
-
-          trace = wrapper.application_trace
-          trace = wrapper.framework_trace if trace.empty?
-          ActiveSupport::Deprecation.silence do
-            message = "\n#{exception.class} (#{exception.message}):\n"
-            message << exception.annoted_source_code.to_s if exception.respond_to?(:annoted_source_code)
-            message << "  " << trace.join("\n  ")
-            logger.fatal("#{message}\n\n")
-          end
-
-          # exception_to_db(exception) # Where are we gonna send exceptions... TODO: hook into Airbrake here if desired
-        end
+        # exception_to_db(exception) # Where are we gonna send exceptions... TODO: hook into Airbrake here if desired
       end
 
     end
