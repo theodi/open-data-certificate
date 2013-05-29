d29c5630c351305e84ee94ed9f153238b7646917
Peter West <pete@whiteoctober.co.uk>
2013-05-29 14:57:15 +0100

    Menus now open and close with javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,13 +22,51 @@
 
 // MIT license
 
-$(function(){
-	// hack
+$(function() {
+
+	// Skrollr data settings
 	var bleed = $('body.odi-bleed').attr({
 		'data-0':'background-position:0 -450px',
 		'data-2500':'background-position:0 1000px',
 	});
 
-	if(bleed.size()) skrollr.init({forceHeight: false});
-})
+	if (bleed.size()) skrollr.init({forceHeight: false});
 
+  // Event to close menus when clicking away off them
+  var openMenu = false;
+  $(document).on('click', function() {
+    if (openMenu) {
+      openMenu.removeClass('active');
+      openMenu.data('target').removeClass('active');
+      openMenu = false;
+    }
+  });
+
+  // Bind related elements for each toggle button
+  $('[data-dropdown=toggle]').each(function() {
+    var self = $(this);
+    self.data('target', $(self.data('target')));
+    self.data('parent', $(self.data('parent')));
+
+    // Event to prevent menus being closed when clicked on them
+    self.data('target').click(function(e) { e.stopPropagation(); });
+  });
+
+  // Trigger dropdown buttons
+  $(document).on('click', '[data-dropdown=toggle]', function() {
+    var self = $(this);
+    var active = !self.data('target').hasClass('active');
+
+    self.data('parent').find('[data-dropdown=toggle]').each(function() {
+      var self = $(this);
+      self.removeClass('active');
+      self.data('target').removeClass('active');
+    });
+
+    self.toggleClass('active', active);
+    self.data('target').toggleClass('active', active);
+    openMenu = active && self;
+
+    return false;
+  });
+});
