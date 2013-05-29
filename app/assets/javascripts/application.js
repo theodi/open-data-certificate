2b759b41dbe44fbcb39ef6e2aed50b838bc5baab
Ben Foxall <benfoxall@gmail.com>
2013-05-29 14:04:43 +0100

    use bootstrap js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,14 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
-
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-
-// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
-
-// MIT license
+//= require twitter/bootstrap
 
 $(function(){
 	// hack
