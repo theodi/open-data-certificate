3f16b0d4c2d506ef22811ac739f02255c07c947a
Peter West <pete@whiteoctober.co.uk>
2013-05-31 16:45:10 +0100

    Removed various unnecessary Removed unnecessary comments from js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,27 +1,7 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
 //= require twitter/bootstrap
-
-
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-
-// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
-
-// MIT license
 
 $(function(){
 	// Skrollr data settings
@@ -77,7 +57,7 @@ $(function(){
 
 
   //////
-  // Questionnaire 
+  // Questionnaire
 
 
   /*
