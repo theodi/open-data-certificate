9832188c432af8381beb890f658dbbdc0e93bccf
Ben Foxall <benfoxall@gmail.com>
2013-06-02 21:31:28 +0100

    also support string_value not equal empty string

--- app/models/dependency_condition.rb
+++ app/models/dependency_condition.rb
@@ -11,14 +11,15 @@ class DependencyCondition < ActiveRecord::Base
   # blank response is nil, rather than ""
   def to_hash(response_set)
 
-    if operator == '==' && string_value == ""
+    if ['==', '!='].include?(operator) && string_value == ""
       is_blank = true
 
       response_set.responses.where(answer_id: answer).each do |response|
         is_blank = false unless response.string_value.blank?
       end
 
-      return {rule_key.to_sym => is_blank}
+      flip = operator == '!='
+      return {rule_key.to_sym => flip ? !is_blank : is_blank}
 
     else
       super response_set
