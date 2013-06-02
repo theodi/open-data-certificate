4698e2061298ffb80834b6534185ae89629f97a3
Ben Foxall <benfoxall@gmail.com>
2013-06-02 21:18:38 +0100

    put in support for {string_value => ""} dependency rules

---
+++ app/models/dependency_condition.rb
@@ -0,0 +1,28 @@
+class DependencyCondition < ActiveRecord::Base
+  unloadable
+  include Surveyor::Models::DependencyConditionMethods
+
+
+  # overriding the to_hash method to add support for:
+  # 
+  #    condition_X :q_name, '==', {:string_value => '', :answer_reference => '1'}
+  #
+  # in the original implementation this doesn't work because a
+  # blank response is nil, rather than ""
+  def to_hash(response_set)
+
+    if operator == '==' && string_value == ""
+      is_blank = true
+
+      response_set.responses.where(answer_id: answer).each do |response|
+        is_blank = false unless response.string_value.blank?
+      end
+
+      return {rule_key.to_sym => is_blank}
+
+    else
+      super response_set
+
+    end
+  end
+end
