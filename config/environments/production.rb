168ed05bef9c60a04f7dfc258cb8f5c55c01f924
pezholio <pezholio@gmail.com>
2013-06-10 17:21:45 +0100

    ENV variable is `MANDRILL_PASSWORD`

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ OpenDataCertificate::Application.configure do
       :port      => 25, # ports 587 and 2525 are also supported with STARTTLS
       :enable_starttls_auto => true, # detects and uses STARTTLS
       :user_name => ENV["MANDRILL_USERNAME"],
-      :password  => ENV["MANDRILL_APIKEY"], # SMTP password is any valid API key
+      :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
       :authentication => 'login', # Mandrill supports 'plain' or 'login'
       # :domain => 'heroku.com', # your domain to identify your server when connecting
     }
