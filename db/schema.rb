0875446e571d5f1088b2dee8d62a96943ed5147c
pezholio <pezholio@gmail.com>
2013-06-12 13:44:48 +0100

    Add error flag to responses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130528163600) do
+ActiveRecord::Schema.define(:version => 20130612101936) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -173,10 +173,11 @@ ActiveRecord::Schema.define(:version => 20130528163600) do
     t.string   "string_value"
     t.string   "response_other"
     t.string   "response_group"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at",                           :null => false
+    t.datetime "updated_at",                           :null => false
     t.integer  "survey_section_id"
     t.string   "api_id"
+    t.boolean  "error",             :default => false
   end
 
   add_index "responses", ["api_id"], :name => "uq_responses_api_id", :unique => true
