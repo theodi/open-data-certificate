7722db772054740683475bfe585e04997ad7e7d1
Ben Foxall <benfoxall@gmail.com>
2013-06-19 23:38:35 +0100

    removed unused functions of the datasets

--- test/functional/datasets_controller_test.rb
+++ test/functional/datasets_controller_test.rb
@@ -1,24 +1,5 @@
 require 'test_helper'
 
 class DatasetsControllerTest < ActionController::TestCase
-  include Devise::TestHelpers
-
-  test "not signed_in users should get redirected to login" do
-    get :new
-    assert_response :redirect
-  end
-
-  test "signed_in users should get new" do
-    sign_in FactoryGirl.create(:user)
-    get :new
-    assert_response :success
-  end
-
-  test "signed_in users should create dataset" do
-    sign_in FactoryGirl.create(:user)
-    assert_difference('Dataset.count') do
-      post :create, dataset: {title: 'my dataset' }
-    end
-  end
-
+  
 end
