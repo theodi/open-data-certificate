ab49b22611499ad376028c0638e53a10cb74dff9
Ben Foxall <benfoxall@gmail.com>
2013-06-20 00:17:52 +0100

    allow response sets to be deleted

---
+++ test/functional/response_sets_controller_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class ResponseSetsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  test "user can destroy their response_set" do
+    @user = FactoryGirl.create(:user)
+    @response_set = FactoryGirl.create(:response_set, :user => @user)
+
+    sign_in @user
+
+    assert_difference('ResponseSet.count', -1) do
+      delete :destroy, use_route: :surveyor, id: @response_set.id
+    end
+
+    assert_redirected_to dashboard_path
+
+  end
+
+
+  test "another user can't destroy response_set" do
+    @response_set = FactoryGirl.create(:response_set)
+
+    # unconnected user
+    sign_in FactoryGirl.create(:user)
+
+    assert_no_difference('ResponseSet.count') do
+      delete :destroy, use_route: :surveyor, id: @response_set.id
+    end
+
+    assert_response 304
+
+  end
+
+
+end
