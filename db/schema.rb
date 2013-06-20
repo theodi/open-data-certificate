02196cb08adee1805d5ad0f423874dc16497dc20
Jo Lankester <jo@whiteoctober.co.uk>
2013-06-20 16:57:14 +0100

    Changed button and added status label

--- db/schema.rb
+++ db/schema.rb
@@ -185,11 +185,10 @@ ActiveRecord::Schema.define(:version => 20130619124058) do
     t.string   "string_value"
     t.string   "response_other"
     t.string   "response_group"
-    t.datetime "created_at",                           :null => false
-    t.datetime "updated_at",                           :null => false
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
     t.integer  "survey_section_id"
     t.string   "api_id"
-    t.boolean  "error",             :default => false
   end
 
   add_index "responses", ["api_id"], :name => "uq_responses_api_id", :unique => true
