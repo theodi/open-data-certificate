043bfac11767ec9f98d5c60010642e76d1abe0c8
Ben Foxall <benfoxall@gmail.com>
2013-07-03 00:45:53 +0100

    deal with trying to publish unpublishable questionnaire

--- app/controllers/response_sets_controller.rb
+++ app/controllers/response_sets_controller.rb
@@ -16,8 +16,13 @@ class ResponseSetsController < ApplicationController
   def publish
     @response_set = ResponseSet.find params[:id]
 
-    if current_user && @response_set.user == current_user && @response_set.publish!
-      redirect_to dashboard_path, :notice => t('dashboard.published_response_set')
+    if current_user && @response_set.user == current_user
+      begin
+        @response_set.publish!
+        redirect_to dashboard_path, :notice => t('dashboard.published_response_set')
+      rescue AASM::InvalidTransition
+        redirect_to dashboard_path, :notice => t('dashboard.unable_to_publish_response_set_invalid')
+      end
     else
       redirect_to dashboard_path, status: 304, :notice => t('dashboard.unable_to_publish_response_set')
     end
