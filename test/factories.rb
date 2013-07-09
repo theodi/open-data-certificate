799bff9a60a681fe8d810505480c126b29535176
Ben Foxall <benfoxall@gmail.com>
2013-07-09 12:22:49 +0100

    fixed syntax error on dashboard response set, and updated test to catch it

--- test/factories.rb
+++ test/factories.rb
@@ -8,9 +8,12 @@ FactoryGirl.define do
     password "testpassword"
     password_confirmation "testpassword"
 
-    factory :user_with_datasets do
+    factory :user_with_responses do
       after(:create) do |user|
         FactoryGirl.create_list(:dataset, 3, user: user)
+        user.datasets.each do |dataset|
+          FactoryGirl.create_list(:response_set, 2, user: user, dataset: dataset)
+        end
       end
     end
   end
