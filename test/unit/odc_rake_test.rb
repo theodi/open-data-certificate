edb44ebd821826473541b1653475db837cea0fa2
Ben Foxall <benfoxall@gmail.com>
2013-07-18 15:04:44 +0100

    added odc:purge_questionnaires rake task

--- test/unit/odc_rake_test.rb
+++ test/unit/odc_rake_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
+require 'rake'
+OpenDataCertificate::Application.load_tasks
 
 class OdcRakeTest < ActiveSupport::TestCase
 
   test "all the surveys in surveys directory parse" do
-    require 'rake'
-    OpenDataCertificate::Application.load_tasks
 
     files = Dir.entries(File.join(Rails.root, 'surveys')).select { |file| file =~ /.*\.rb/ }
 
@@ -29,6 +29,25 @@ class OdcRakeTest < ActiveSupport::TestCase
     # ensure the hash of exceptions caught from parsing the survey files has no values. If it does, join all the execptions
     # together with their filename to aid bugfixing
     assert @result.values.none?, @result.delete_if{|k,v|v.blank?}.map {|k,v| ["survey: #{k}", "#{v}\n"] }.unshift("\n").join("\n")
+  end
+
+
+  test "purge_questionnaires gets rid of unanswered questionnaires" do
+
+    yesterday =  Time.now - 24.hours
+
+    @a = FactoryGirl.create :response_set, user: nil
+    @b = FactoryGirl.create :response_set, updated_at: yesterday, user: nil
+    @c = FactoryGirl.create :response_set, updated_at: yesterday, user: FactoryGirl.create(:user)
+
+    assert_difference 'ResponseSet.count', -1 do
+      Rake::Task["odc:purge_questionnaires"].invoke
+    end
+
+    assert ResponseSet.exists? @a
+    assert_false ResponseSet.exists? @b
+    assert ResponseSet.exists? @c
+
   end
   
 end
