36f2073f80352929444bbc14c5b5cd4680f4564c
Ben Foxall <benfoxall@gmail.com>
2013-07-19 14:31:05 +0100

    installed delayed job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130702204542) do
+ActiveRecord::Schema.define(:version => 20130719130347) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -67,6 +67,22 @@ ActiveRecord::Schema.define(:version => 20130702204542) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
   end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "dependencies", :force => true do |t|
     t.integer  "question_id"
