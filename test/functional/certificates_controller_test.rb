a4fc38f784e68d1a639b4862f7616f17266dfbb0
pezholio <pezholio@gmail.com>
2013-07-31 14:32:42 +0100

    Add specific :certificate_with_dataset factory, and remove dataset association from :response_set
    This broke the tests

--- test/functional/certificates_controller_test.rb
+++ test/functional/certificates_controller_test.rb
@@ -4,13 +4,13 @@ class CertificatesControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
   test "published certificates can be shown" do
-    cert = FactoryGirl.create(:published_certificate)
+    cert = FactoryGirl.create(:published_certificate_with_dataset)
     get :show, {dataset_id: cert.response_set.dataset.id, id: cert.id}
     assert_response :success
   end
 
   test "unpublished certificates can't be shown" do
-    cert = FactoryGirl.create(:certificate)
+    cert = FactoryGirl.create(:certificate_with_dataset)
     get :show, {dataset_id: cert.response_set.dataset.id, id: cert.id}
     assert_response 404
   end
@@ -19,7 +19,7 @@ class CertificatesControllerTest < ActionController::TestCase
     user = FactoryGirl.create(:user)
     sign_in user
   
-    cert = FactoryGirl.create(:certificate)
+    cert = FactoryGirl.create(:certificate_with_dataset)
     cert.response_set.assign_to_user! user
     
     get :show, {dataset_id: cert.response_set.dataset.id, id: cert.id}
