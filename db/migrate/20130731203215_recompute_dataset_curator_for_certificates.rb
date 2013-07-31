2d2b5c295903efbdfa5329f4c0f6e64386946399
Ben Foxall <benfoxall@gmail.com>
2013-07-31 22:39:53 +0100

    corrected migration to deal with orphan certificates

--- db/migrate/20130731203215_recompute_dataset_curator_for_certificates.rb
+++ db/migrate/20130731203215_recompute_dataset_curator_for_certificates.rb
@@ -3,7 +3,9 @@ class RecomputeDatasetCuratorForCertificates < ActiveRecord::Migration
     puts "...this may take some time\n"
     # this may still result in curator:nil for some certificates (where it's not supplied, or
     # there isn't a matching question on the survey)
-    Certificate.where(curator:nil).find_each(batch_size: 20) {|c| c.update_from_response_set}
+    Certificate.where(curator: nil).find_each(batch_size: 20) do |c| 
+      c.update_from_response_set unless c.response_set.nil?
+    end
   end
 
   def down
