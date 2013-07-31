b32868a53c43f14c0ef23677c81234784fef819d
Ben Foxall <benfoxall@gmail.com>
2013-07-31 21:43:03 +0100

    added migrations so that all the current certificates will have a curator set

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130724115538) do
+ActiveRecord::Schema.define(:version => 20130731203215) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
