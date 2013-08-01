547e845015bfb32842417aa196ebea517a297596
Ben Foxall <benfoxall@gmail.com>
2013-08-01 18:54:49 +0100

    removed migration that is failing on production

--- db/migrate/20130731203215_recompute_dataset_curator_for_certificates.rb
+++ db/migrate/20130731203215_recompute_dataset_curator_for_certificates.rb
@@ -1,11 +1,13 @@
 class RecomputeDatasetCuratorForCertificates < ActiveRecord::Migration
   def up
-    puts "...this may take some time\n"
+    # REMOVED - https://theodi.airbrake.io/groups/64547582
+
+    # puts "...this may take some time\n"
     # this may still result in curator:nil for some certificates (where it's not supplied, or
     # there isn't a matching question on the survey)
-    Certificate.where(curator: nil).find_each(batch_size: 20) do |c| 
-      c.update_from_response_set unless c.response_set.nil?
-    end
+    # Certificate.where(curator: nil).find_each(batch_size: 20) do |c| 
+    #   c.update_from_response_set unless c.response_set.nil?
+    # end
   end
 
   def down
