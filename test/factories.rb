6b4864f8675d3efa8974a7b1abe328bb4246ae9f
pezholio <pezholio@gmail.com>
2013-08-02 11:08:09 +0100

    Add documentation URL stuff to Dataset test

--- test/factories.rb
+++ test/factories.rb
@@ -20,9 +20,14 @@ FactoryGirl.define do
 
   factory :dataset do
     title "Test dataset"
+    documentation_url "http://www.example.com"
 
     factory :untitled_dataset do
       title nil
+    end
+    
+    factory :dataset_without_documentation_url do
+      documentation_url nil
     end
   end
 
