d5e438fb46c5837bb288b1b52730f65314c53eb7
Ben Foxall <benfoxall@gmail.com>
2013-08-05 18:52:06 +0100

    moved certificate#index test to dataset#index and made change so it isn't limited to logged in users

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,7 +1,7 @@
 class DatasetsController < ApplicationController
   load_and_authorize_resource
 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: :dashboard
 
   def index
     @certificates = Certificate.where(:published => true).by_newest
