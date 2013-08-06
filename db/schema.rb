93ac68a32f44602aabd2e63a0c81d8bb0fa8cd87
pezholio <pezholio@gmail.com>
2013-08-06 20:49:27 +0100

    Change existing licence identifiers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130805162301) do
+ActiveRecord::Schema.define(:version => 20130806160042) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
