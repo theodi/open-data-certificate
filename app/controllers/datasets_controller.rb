9be4c905fee2b72b21662338677fb22d632f1b25
pezholio <pezholio@gmail.com>
2013-08-08 20:32:00 +0100

    Add respond_to blocks to controller methods
    Stops 500 errors when accept headers explicitly sent

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -13,15 +13,27 @@ class DatasetsController < ApplicationController
         @certificates.search_country(params[:search])
       ].flatten.uniq).page params[:page]
     end
+    
+    respond_to do |format|
+      format.html
+    end
   end
 
   def dashboard
     @datasets = current_user.try(:datasets) || []
     @surveys = Survey.available_to_complete
+    
+    respond_to do |format|
+      format.html
+    end
   end
 
   def show
     @dataset = Dataset.find(params[:id])
     @certificates = @dataset.certificates.where(:published => true).by_newest
+    
+    respond_to do |format|
+      format.html
+    end
   end
 end
