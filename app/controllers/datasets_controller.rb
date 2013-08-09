edd9e20d1ecb3ed20d906b281e387e875a5a4f4f
Ben Foxall <benfoxall@gmail.com>
2013-08-09 19:12:25 +0100

    formatting typeahead responses and avoiding null publishers

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -28,7 +28,8 @@ class DatasetsController < ApplicationController
           }
         end
       when 'publisher'
-        Dataset.search({curator_cont:params[:q]}).result
+        Certificate.search({curator_cont:params[:q]}).result
+                .where(Certificate.arel_table[:curator].not_eq(nil))
                 .group(:curator)
                 .limit(5).map do |dataset|
           {
@@ -39,7 +40,7 @@ class DatasetsController < ApplicationController
       when 'country'
         Survey.search({full_title_cont:params[:q]}).result.limit(5).map do |survey|
           {
-            value: survey.full_title,
+            value: survey.full_title || survey.title,
             path: datasets_path(jurisdiction:survey.title)
           }
         end
