c77cabfc0d7ade7b73c6c1b5a02791ee4f14bbe7
Peter West <pete@whiteoctober.co.uk>
2013-08-09 17:51:17 +0100

    Fixed survey status migration

--- db/migrate/20130807111430_add_status_to_survey.rb
+++ db/migrate/20130807111430_add_status_to_survey.rb
@@ -1,11 +1,5 @@
 class AddStatusToSurvey < ActiveRecord::Migration
-  def up
+  def change
     add_column :surveys, :status, :string, default: 'alpha'
-
-    Survey.where(title: 'GB').update_all(status: 'beta')
-  end
-
-  def down
-    remove_column :surveys, :status
   end
 end
