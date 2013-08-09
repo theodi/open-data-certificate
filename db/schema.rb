7d9da16aa471de4bf97bc1fa6e05d4803574bc31
Ben Foxall <benfoxall@gmail.com>
2013-08-09 16:19:33 +0100

    added migration to generate curators by delayed job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130806160042) do
+ActiveRecord::Schema.define(:version => 20130809150653) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
