e71f3ed0bd11075a4b9ed7a523b0a9631d7fe649
Ben Foxall <benfoxall@gmail.com>
2013-08-15 20:01:44 +0100

    send the access_code for the map

--- app/controllers/jurisdictions_controller.rb
+++ app/controllers/jurisdictions_controller.rb
@@ -1,7 +1,7 @@
 class JurisdictionsController < ApplicationController
   def index
     @jurisdictions = Survey.available_to_complete
-                           .select([:title, :full_title, :status])
+                           .select([:title, :full_title, :status, :access_code])
                            .map &:attributes
 
     render json: @jurisdictions
