e1ceac45f5bbeb50e1b1e6ed2d08bf8c95a51fc0
theponti <cjponti@gmail.com>
2013-08-16 10:39:29 +0100

    Refactored FactoryGirl factories into their own files in new test/factories directory

--- test/unit/survey_test.rb
+++ test/unit/survey_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class SurveyTest < ActiveSupport::TestCase
-
   test "#superceded? returns false when survey is most recent version" do
     survey = FactoryGirl.create :survey # version 0
     survey = FactoryGirl.create :survey, access_code: survey.access_code, survey_version: nil # version 1
@@ -19,5 +18,10 @@ class SurveyTest < ActiveSupport::TestCase
     assert survey.superceded?
   end
 
+  # test "#mandatory_questions returns array of mandatory questions for survey" do
+  #   survey = FactoryGirl.create :survey_with_2_questions
+  #   binding.pry
+  #   assert_equal survey.mandatory_questions.count, 1
+  # end
 
 end
