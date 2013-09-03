d724d3b579b0e0f8e0184eaf19a3c3fb476c556e
Ben Foxall <benfoxall@gmail.com>
2013-09-03 15:48:00 +0100

    support pick values by default for response_set#value_for

--- app/models/response_set.rb
+++ app/models/response_set.rb
@@ -331,7 +331,7 @@ class ResponseSet < ActiveRecord::Base
 
   # finds the string value for a given response_identifier
   private
-  def value_for reference_identifier, value = :string_value
+  def value_for reference_identifier, value = :to_s
     responses.joins(:question).where(questions: {reference_identifier: survey.meta_map[reference_identifier]}).first.try(value)
   end
 
