27044cc52792733813831cfa4968ffc16266c53a
pezholio <pezholio@gmail.com>
2013-09-06 16:35:38 +0100

    Move logic from Rake task into module

--- lib/tasks/data_dump.rb
+++ lib/tasks/data_dump.rb
@@ -1,37 +1,87 @@
 require 'fog'
 
-def service
-  Fog::Storage.new({
-      :provider            => 'Rackspace',
-      :rackspace_username  => ENV['RACKSPACE_USERNAME'],
-      :rackspace_api_key   => ENV['RACKSPACE_API_KEY'],
-      :rackspace_auth_url  => Fog::Rackspace::UK_AUTH_ENDPOINT,
-      :rackspace_region    => :lon
-  })
-end
-
-def av
-  av = ActionView::Base.new
-  av.view_paths = ActionController::Base.view_paths
-
-  class << av
-      routes = Rails.application.routes
-      routes.default_url_options = {:host => 'certificates.theodi.org', :protocol => 'https'}
-      include routes.url_helpers
+module DataDump
+  
+  def self.current
+    a = []
+      
+    certs = Certificate.where(:published => true)
+    certs.each do |cert|
+      a << build_item(cert)
+    end
+  
+    json = "{#{a.join(',')}}"
+  
+    upload(json)
   end
-  av
-end
+  
+  def self.latest
+    new = 0
+    updated = 0
+    
+    dir = service.directories.get ENV['RACKSPACE_CERTIFICATE_DUMP_CONTAINER']
+    file = dir.files.head "certificates.json"
+    
+    certs = Certificate.where(:published => true).
+              where("updated_at > ?", file.last_modified)
+    
+    if certs.count > 0
+      json = JSON.parse(dir.files.get("certificates.json").body)
+      # Loop through results
+      certs.each do |cert|
+        url = av.dataset_certificate_url(cert.dataset, cert)
+        if json[url].nil?
+          # If certificate doesn't exist, add it
+          json << JSON.parse("{"+ build_item(cert) +"}")
+          new += 1
+        else
+          # If not, update what's already there
+          json[url] = JSON.parse("{"+ build_item(cert) +"}")
+          updated += 1
+        end
+      end
+      upload(json.to_s)
+      puts "#{new} certificates added"
+      puts "#{updated} certificates updated"
+    end
+  end
+  
+  private
 
-def build_item(certificate)
-  json = "\"#{av.dataset_certificate_url(certificate.dataset, certificate)}\":"
-  json << av.render(
-      :file => 'certificates/_certificate.json', 
-      :layout => nil, 
-      :locals => { :cert => certificate }
-      )
-end
+  def self.service
+    Fog::Storage.new({
+        :provider            => 'Rackspace',
+        :rackspace_username  => ENV['RACKSPACE_USERNAME'],
+        :rackspace_api_key   => ENV['RACKSPACE_API_KEY'],
+        :rackspace_auth_url  => Fog::Rackspace::UK_AUTH_ENDPOINT,
+        :rackspace_region    => :lon
+    })
+  end
 
-def upload(json)
-  dir = service.directories.get ENV['RACKSPACE_CERTIFICATE_DUMP_CONTAINER']
-  dir.files.create :key => "certificates.json", :body => json
+  def self.av
+    av = ActionView::Base.new
+    av.view_paths = ActionController::Base.view_paths
+
+    class << av
+        routes = Rails.application.routes
+        routes.default_url_options = {:host => 'certificates.theodi.org', :protocol => 'https'}
+        include routes.url_helpers
+    end
+    av
+  end
+
+  def self.build_item(certificate)
+    json = "\"#{av.dataset_certificate_url(certificate.dataset, certificate)}\":"
+    json << av.render(
+        :file => 'certificates/_certificate.json', 
+        :layout => nil, 
+        :locals => { :cert => certificate }
+        )
+  end
+
+  def self.upload(json)
+    dir = service.directories.get ENV['RACKSPACE_CERTIFICATE_DUMP_CONTAINER']
+    dir.files.create :key => "certificates.json", :body => json
+  end
+
 end
--- lib/tasks/dump.rake
+++ lib/tasks/dump.rake
@@ -6,48 +6,12 @@ namespace :dump do
     
     desc "Dump data for certificates all current certificates"
     task :all_current => :environment do        
-      a = []
-        
-      certs = Certificate.where(:published => true)
-      certs.each do |cert|
-        a << build_item(cert)
-      end
-    
-      json = "{#{a.join(',')}}"
-    
-      upload(json)
+      DataDump.current
     end
   
     desc "Get the latest data and add it to the dump"
     task :latest => :environment do
-      new = 0
-      updated = 0
-      
-      dir = service.directories.get ENV['RACKSPACE_CERTIFICATE_DUMP_CONTAINER']
-      file = dir.files.head "certificates.json"
-      
-      certs = Certificate.where(:published => true).
-                where("updated_at > ?", file.last_modified)
-      
-      if certs.count > 0
-        json = JSON.parse(dir.files.get("certificates.json").body)
-        # Loop through results
-        certs.each do |cert|
-          url = av.dataset_certificate_url(cert.dataset, cert)
-          if json[url].nil?
-            # If certificate doesn't exist, add it
-            json << JSON.parse("{"+ build_item(cert) +"}")
-            new += 1
-          else
-            # If not, update what's already there
-            json[url] = JSON.parse("{"+ build_item(cert) +"}")
-            updated += 1
-          end
-        end
-        upload(json.to_s)
-        puts "#{new} certificates added"
-        puts "#{updated} certificates updated"
-      end
+      DataDump.latest
     end
   
   end
