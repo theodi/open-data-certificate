6c9a6d9d65e7c559d43277b1dca08eaf096026e3
Ben Foxall <benfoxall@gmail.com>
2013-09-12 15:53:03 +0100

    updated translations for development survey

--- surveys/development/languages.rb
+++ surveys/development/languages.rb
@@ -1,5 +1,5 @@
 # encoding: UTF-8
-survey "dv1", :full_title => 'Language Test (dv1)', :dataset_title => 'dataTitle', :dataset_curator => 'dataTitle' do
+survey "dv1", :full_title => 'Language Test (dv1)', :dataset_title => 'dataTitle', :dataset_curator => 'dataTitle', default_locale_name: 'English' do
   translations :en =>:default, :es => "../translations/languages.es.yml", :he => "../translations/languages.he.yml", :ko => "../translations/languages.ko.yml"
 
   section_one "One" do
