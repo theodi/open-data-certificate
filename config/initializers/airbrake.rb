74635bd7506ae647c7954cef6a8b4147690a1e62
Ben Foxall <benfoxall@gmail.com>
2013-09-13 14:20:19 +0100

    display airbrake error id in 500 page

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,8 @@
 if ENV['AIRBRAKE_CERTIFICATE_KEY']
   Airbrake.configure do |config|
     config.api_key = ENV['AIRBRAKE_CERTIFICATE_KEY']
+
+    # displayed in the 500.html error page
+    config.user_information = "Exception ID <strong>{{ error_id }}</strong>"
   end
 end
