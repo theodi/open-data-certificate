71bb5bc54251f7eb93fcc841440fa8072f7c2ec0
Ben Foxall <benfoxall@gmail.com>
2013-09-26 16:34:31 +0100

    refactored abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,19 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, ResponseSet do |response_set|
-      response_set.nil? ||
-        (
-        response_set.user.nil? ||
-        response_set.user == user
-        )
-    end
+    
+    can :manage, ResponseSet, user_id: nil
+    can :manage, ResponseSet, user_id: user.id
 
-    can :manage, Dataset do |dataset|
-      dataset.user.nil? ||
-        dataset.user == user
-    end
-
+    can :manage, Dataset, user_id: nil
+    can :manage, Dataset, user_id: user.id
     can :read, Dataset
 
   end
