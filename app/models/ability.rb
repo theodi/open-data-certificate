5f80c5a233737ddbc229e87a77f28553cfa0586d
Ben Foxall <benfoxall@gmail.com>
2013-09-26 17:08:36 +0100

    reverted ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,19 @@ class Ability
 
   def initialize(user)
     
-    can :manage, ResponseSet, user_id: nil
-    can :manage, ResponseSet, user_id: user.id
+    can :manage, ResponseSet do |response_set|
+      response_set.nil? ||
+        (
+        response_set.user.nil? ||
+        response_set.user == user
+        )
+    end
+    
+    can :manage, Dataset do |dataset|
+      dataset.user.nil? ||
+        dataset.user == user
+    end
 
-    can :manage, Dataset, user_id: nil
-    can :manage, Dataset, user_id: user.id
     can :read, Dataset
 
   end
