bd5147adab9df2337f93643740d940bbb23eda40
Peter West <pete@whiteoctober.co.uk>
2013-10-08 11:51:15 +0100

    Added filter method

--- app/assets/javascripts/polyfill.js
+++ app/assets/javascripts/polyfill.js
@@ -134,3 +134,31 @@ if (!Array.prototype.forEach) {
 if (!Array.prototype.each) {
   Array.prototype.each = Array.prototype.forEach;
 }
+
+if (!Array.prototype.filter) {
+  Array.prototype.filter = function(fun /*, thisp*/) {
+    'use strict';
+
+    if (!this) {
+      throw new TypeError();
+    }
+
+    var objects = Object(this);
+    var len = objects.length >>> 0;
+    if (typeof fun !== 'function') {
+      throw new TypeError();
+    }
+
+    var res = [];
+    var thisp = arguments[1];
+    for (var i in objects) {
+      if (objects.hasOwnProperty(i)) {
+        if (fun.call(thisp, objects[i], i, objects)) {
+          res.push(objects[i]);
+        }
+      }
+    }
+
+    return res;
+  };
+}
