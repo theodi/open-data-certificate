a4b1e568cd2896f32a644eddbfbac3b3c992d82f
Ben Foxall <benfoxall@gmail.com>
2013-10-10 16:13:03 +0100

    TEMPORARY - hard code juvia staging config

--- config/initializers/juvia_rails.rb
+++ config/initializers/juvia_rails.rb
@@ -1,5 +1,5 @@
 JuviaRails.configure do |config|
-  config.server_url    = ENV['JUVIA_SERVER_URL']
-  config.site_key      = ENV['JUVIA_SITE_KEY']
+  config.server_url    = 'http://juvia.theodi.org' #ENV['JUVIA_SERVER_URL']
+  config.site_key      = '70gibwaloj9uzoefjpkzjvo9p9gsjmv' #ENV['JUVIA_SITE_KEY']
   config.comment_order = 'earliest-first'
 end
