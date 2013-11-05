e15e68742cf313143b956587e22bb51cbf6f0903
Ben Foxall <benfoxall@gmail.com>
2013-11-05 14:40:57 +0000

    upped dev event message limit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131031124226) do
+ActiveRecord::Schema.define(:version => 20131105143314) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -122,9 +122,9 @@ ActiveRecord::Schema.define(:version => 20131031124226) do
   add_index "dependency_conditions", ["dependency_id"], :name => "i_dependency_conditions_on_dependencies_question_id"
 
   create_table "dev_events", :force => true do |t|
-    t.string   "message"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.text     "message",    :limit => 65535
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
   end
 
   create_table "kitten_data", :force => true do |t|
