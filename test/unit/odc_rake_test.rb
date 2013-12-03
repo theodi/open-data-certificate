0883858c9899e5d00eb4e4790eb88ea9123f5df4
Ben Foxall <benfoxall@gmail.com>
2013-12-03 13:57:11 +0000

    test for deferred jobs

--- test/unit/odc_rake_test.rb
+++ test/unit/odc_rake_test.rb
@@ -160,11 +160,14 @@ class OdcRakeTest < ActiveSupport::TestCase
       end
     end
 
+    assert_no_difference 'Survey.count', "surveys were queued" do
+      Delayed::Worker.new({exit_on_complete: true}).start
+    end
+
+    # run all jobs at once
+    Delayed::Job.update_all run_at: 1.minute.ago
+
     assert_difference 'Survey.count', 3, "surveys were generated" do
-
-      # run all jobs at once
-      Delayed::Job.update_all run_at: 1.minute.ago
-
       Delayed::Worker.new({exit_on_complete: true}).start
     end
 
