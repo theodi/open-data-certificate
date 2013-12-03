295017c3ff3dd45f26fea96d64092218e58b3c1c
Ben Foxall <benfoxall@gmail.com>
2013-12-03 13:26:01 +0000

    correct marking tasks to run instantly

--- test/unit/odc_rake_test.rb
+++ test/unit/odc_rake_test.rb
@@ -161,6 +161,10 @@ class OdcRakeTest < ActiveSupport::TestCase
     end
 
     assert_difference 'Survey.count', 3, "surveys were generated" do
+
+      # run all jobs at once
+      Delayed::Job.update_all run_at: 1.minute.ago
+
       Delayed::Worker.new({exit_on_complete: true}).start
     end
 
