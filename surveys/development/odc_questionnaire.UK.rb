ea57608fba3eca8fe3f3aef5dca693f49b674835
Ben Foxall <ben@whiteoctober.co.uk>
2014-04-17 14:11:46 +0100

    rename uk-ogl to ogl-uk

--- surveys/development/odc_questionnaire.UK.rb
+++ surveys/development/odc_questionnaire.UK.rb
@@ -351,7 +351,7 @@ survey 'GB',
       :text_as_statement => 'Open Data Commons Public Domain Dedication and Licence (PDDL)'
     a_cc_zero 'Creative Commons CCZero',
       :text_as_statement => 'Creative Commons CCZero'
-    a_uk_ogl 'UK Open Government Licence',
+    a_ogl_uk 'UK Open Government Licence',
       :text_as_statement => 'UK Open Government Licence'
     a_na 'Not applicable',
       :text_as_statement => ''
@@ -463,7 +463,7 @@ survey 'GB',
       :text_as_statement => 'Creative Commons Attribution Share-Alike'
     a_cc_zero 'Creative Commons CCZero',
       :text_as_statement => 'Creative Commons CCZero'
-    a_uk_ogl 'UK Open Government Licence',
+    a_ogl_uk 'UK Open Government Licence',
       :text_as_statement => 'UK Open Government Licence'
     a_na 'Not applicable',
       :text_as_statement => ''
