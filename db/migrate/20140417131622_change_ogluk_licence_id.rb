04fb9fd94309824f081ce5ef9a49d73dd540bda1
Ben Foxall <ben@whiteoctober.co.uk>
2014-04-17 15:41:37 +0100

    syntax changes to make the hound happy

--- db/migrate/20140417131622_change_ogluk_licence_id.rb
+++ db/migrate/20140417131622_change_ogluk_licence_id.rb
@@ -1,20 +1,20 @@
 class ChangeOglukLicenceId < ActiveRecord::Migration
   def up
-    data = Answer.joins(:question).where(questions: {:reference_identifier => 'dataLicence'}).readonly(false)
-    
+    data = Answer.joins(:question).where(questions: {reference_identifier: 'dataLicence'}).readonly(false)
+
     data.each do |d|
       case d.reference_identifier
-      when "uk_ogl"
-        d.update_attributes(:reference_identifier => "ogl_uk")
+      when 'uk_ogl'
+        d.update_attributes(:reference_identifier => 'ogl_uk')
       end
     end
-    
-    content = Answer.joins(:question).where(questions: {:reference_identifier => 'contentLicence'}).readonly(false)
-    
+
+    content = Answer.joins(:question).where(questions: {reference_identifier: 'contentLicence'}).readonly(false)
+
     content.each do |c|    
       case c.reference_identifier
-      when "uk_ogl"
-        c.update_attributes(:reference_identifier => "ogl_uk")
+      when 'uk_ogl'
+        c.update_attributes(reference_identifier: 'ogl_uk')
       end
     end
   end
