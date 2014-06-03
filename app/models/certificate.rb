d68bae4d17e5b5cece36f1656a9544ca8108b7f3
pezholio <pezholio@gmail.com>
2014-06-03 13:52:52 +0100

    Use presenters for usage data

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -75,40 +75,8 @@ class Certificate < ActiveRecord::Base
       }
     end
 
-    def published_certificates
-      self.where(published: true).map do |certificate|
-        {
-          name: certificate.name,
-          publisher: certificate.curator,
-          created: certificate.created_at,
-          user: certificate.user.email,
-          country: certificate.survey.title,
-          type: certificate.survey.status,
-          level: certificate.attained_level,
-          verification_type: certificate.certification_type,
-          date_verified: certificate.verifications.count == 0 ? nil : certificate.verifications.last.updated_at
-        }
-      end
-    end
-
-    def all_certificates
-      Certificate.all.map do |certificate|
-        begin
-          {
-            name: certificate.name,
-            publisher: certificate.curator,
-            user_email: certificate.user.nil? ? "N/A" : certificate.user.email,
-            user_name: "#{certificate.user.first_name} #{certificate.user.last_name}",
-            created: certificate.created_at,
-            last_edited: certificate.updated_at,
-            country: certificate.survey.title,
-            status: certificate.published? ? "published" : "draft",
-            level: certificate.attained_level,
-          }
-        rescue
-          nil
-        end
-      end
+    def published
+      self.where(published: true)
     end
 
     def set_expired(surveys)
