11d67e97533e57d253be2991193ac4af5ac00172
pezholio <pezholio@gmail.com>
2014-07-11 12:00:15 +0100

    Use spark

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,40 +1,46 @@
-require 'coveralls'
-Coveralls.wear! 'rails'
+require 'rubygems'
+require 'spork'
 
-ENV["RAILS_ENV"] = "test"
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+Spork.prefork do
+  require 'coveralls'
+  Coveralls.wear! 'rails'
 
-require 'factory_girl'
-require 'vcr'
+  ENV["RAILS_ENV"] = "test"
+  require File.expand_path('../../config/environment', __FILE__)
+  require 'rails/test_help'
 
-VCR.configure do |c|
-  # Automatically filter all secure details that are stored in the environment
-  ignore_env = %w{SHLVL RUNLEVEL GUARD_NOTIFY DRB COLUMNS USER LOGNAME LINES TERM_PROGRAM_VERSION}
-  (ENV.keys-ignore_env).select{|x| x =~ /\A[A-Z_]*\Z/}.each do |key|
-    c.filter_sensitive_data("<#{key}>") { ENV[key] }
-  end
-  c.cassette_library_dir = 'fixtures/cassettes'
-  c.default_cassette_options = { :record => :once }
-  c.hook_into :webmock
-  c.ignore_hosts 'licenses.opendefinition.org', 'www.example.com'
+  require 'factory_girl'
+  require 'vcr'
+  require 'mocha/setup'
 end
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  # fixtures :all
+Spork.each_run do
+  VCR.configure do |c|
+    # Automatically filter all secure details that are stored in the environment
+    ignore_env = %w{SHLVL RUNLEVEL GUARD_NOTIFY DRB COLUMNS USER LOGNAME LINES TERM_PROGRAM_VERSION}
+    (ENV.keys-ignore_env).select{|x| x =~ /\A[A-Z_]*\Z/}.each do |key|
+      c.filter_sensitive_data("<#{key}>") { ENV[key] }
+    end
+    c.cassette_library_dir = 'fixtures/cassettes'
+    c.default_cassette_options = { :record => :once }
+    c.hook_into :webmock
+    c.ignore_hosts 'licenses.opendefinition.org', 'www.example.com'
+  end
 
-  # Add more helper methods to be used by all tests here...
+  class ActiveSupport::TestCase
+    # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+    #
+    # Note: You'll currently still have to declare fixtures explicitly in integration tests
+    # -- they do not yet inherit this setting
+    # fixtures :all
 
-  ENV['RACKSPACE_CERTIFICATE_DUMP_CONTAINER'] = "certificates_test"
+    # Add more helper methods to be used by all tests here...
 
-  def assert_attribute_exists(model, attribute)
-    assert_respond_to model, attribute
+    ENV['RACKSPACE_CERTIFICATE_DUMP_CONTAINER'] = "certificates_test"
+
+    def assert_attribute_exists(model, attribute)
+      assert_respond_to model, attribute
+    end
   end
 
 end
-
-require 'mocha/setup'
