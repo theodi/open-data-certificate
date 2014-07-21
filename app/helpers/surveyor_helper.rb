0d5eb8aa70726d70966702ad773589b1b5113883
pezholio <pezholio@gmail.com>
2014-07-21 12:24:59 +0100

    Don't always compute sibling responses

--- app/helpers/surveyor_helper.rb
+++ app/helpers/surveyor_helper.rb
@@ -17,12 +17,7 @@ module SurveyorHelper
   # Debug response_for method
   def response_for(response_set, responses, question, answer = nil, response_group = nil)
     return nil unless responses && question && question.id
-
     result = responses.detect{|r| (r.question_id == question.id) && (answer.blank? ? true : r.answer_id == answer.id) && (r.response_group.blank? ? true : r.response_group.to_i == response_group.to_i)}
-    return result unless result.blank?
-
-    new_response = response_set.responses.build(:question_id => question.id, :response_group => response_group)
-    new_response.compute_sibling_responses(responses)
-    new_response
+    result ? result : response_set.responses.build(:question_id => question.id, :response_group => response_group)
   end
 end
