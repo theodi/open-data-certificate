47df56ec8a1c2b2544460076634a47c8e0420cea
pezholio <pezholio@gmail.com>
2014-09-09 17:11:00 +0100

    Generate a CSV of all embeds

--- app/models/embed_stat.rb
+++ app/models/embed_stat.rb
@@ -4,4 +4,15 @@ class EmbedStat < ActiveRecord::Base
   attr_accessible :referer
 
   validates :referer, url: true
+
+  def self.csv
+    CSV.generate(row_sep: "\r\n") do |csv|
+      csv << ["Dataset Name", "Dataset URL", "Referring URL", "First Detected"]
+      all.each { |s| csv << [s.dataset.title, s.dataset_path, s.referer, s.created_at] }
+    end
+  end
+
+  def dataset_path
+    Rails.application.routes.url_helpers.dataset_url(dataset, host: OpenDataCertificate::Application.config.action_mailer[:default_url_options][:host])
+  end
 end
--- test/unit/embed_stat.rb
+++ test/unit/embed_stat.rb
@@ -16,4 +16,30 @@ class EmbedStatTest < ActiveSupport::TestCase
     assert_equal e.errors.first, [:referer, "is not a valid URL"]
   end
 
+  test "should generate a CSV" do
+    5.times.each do |n|
+      dataset = FactoryGirl.create(:dataset, title: "Dataset #{n}")
+      2.times.each { |i| dataset.register_embed("http://example#{n}.com/#{i}") }
+    end
+
+    validator = Csvlint::Validator.new(StringIO.new(EmbedStat.csv))
+
+    assert validator.valid?
+
+    csv = CSV.parse(EmbedStat.csv)
+
+    assert_equal csv.count, 11
+    assert_equal csv.first, ["Dataset Name", "Dataset URL", "Referring URL", "First Detected"]
+
+    assert_equal csv[1][0], "Dataset 0"
+    assert_equal csv[1][1], "http://test.dev/datasets/1"
+    assert_equal csv[1][2], "http://example0.com/0"
+    assert_equal Date.parse(csv[1][3]), Date.today
+
+    assert_equal csv.last[0], "Dataset 4"
+    assert_equal csv.last[1], "http://test.dev/datasets/5"
+    assert_equal csv.last[2], "http://example4.com/1"
+    assert_equal Date.parse(csv.last[3]), Date.today
+  end
+
 end
