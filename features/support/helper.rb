42b77b55be0f4588a2cdc8ce2065d10ccd3d382e
pezholio <pezholio@gmail.com>
2014-09-12 15:48:43 +0100

    Add coveralls

--- features/support/helper.rb
+++ features/support/helper.rb
@@ -1,4 +1,6 @@
 require 'webmock/cucumber'
+require 'coveralls'
+Coveralls.wear_merged! 'rails'
 
 def load_fixture(file)
   File.read( File.join( File.dirname(File.realpath(__FILE__)) , "fixtures", file ) )
