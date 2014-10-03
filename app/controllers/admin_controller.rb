3f940cea83047862c05172dfb8005ae5d18ff2cb
pezholio <pezholio@gmail.com>
2014-10-03 11:51:34 +0100

    Bust cache before filter

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,18 @@
 class AdminController < ApplicationController
 
+  before_filter :set_cache_buster
+
   def index
     authorize! :manage, :all
     @title = "Admin"
-
   end
+
+  private
+
+    def set_cache_buster
+      response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    end
 
 end
