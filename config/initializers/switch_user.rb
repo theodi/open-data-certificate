c14ea0ad09a2ee064b3808ae4cf5f689ee82285b
pezholio <pezholio@gmail.com>
2014-10-03 09:26:17 +0100

    Make sure view helpers work in production

--- config/initializers/switch_user.rb
+++ config/initializers/switch_user.rb
@@ -1,6 +1,7 @@
 SwitchUser.setup do |config|
   config.available_users = { :user => lambda { User.order(:email) } }
   config.controller_guard = lambda { |current_user, request| current_user.try(:admin?) }
+  config.view_guard = lambda { |current_user, request| current_user.try(:admin?) }
   config.redirect_path = lambda { |request, params| '/users/dashboard' }
   config.helper_with_guest = false
 end
