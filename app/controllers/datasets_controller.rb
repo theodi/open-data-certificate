3e4ae1f272c15ecec39b266c915244cf9e40ac35
Adam Sven Johnson <adam@pkqk.net>
2014-11-13 12:46:17 +0000

    If no results in search use now as modified date.

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -74,7 +74,7 @@ class DatasetsController < ApplicationController
         .result
     end
 
-    @last_modified_date = datasets.maximum('response_sets.updated_at')
+    @last_modified_date = datasets.maximum('response_sets.updated_at') || Time.current
 
     if stale?(last_modified: @last_modified_date)
       @datasets = datasets.page params[:page]
