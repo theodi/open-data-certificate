75197c1a75a6a7dbc6d33b43c280e4bcf2c268c8
Adam Sven Johnson <adam@pkqk.net>
2014-11-27 16:55:36 +0000

    Confirm users who have already used the service

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141127154117) do
+ActiveRecord::Schema.define(:version => 20141127164041) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
