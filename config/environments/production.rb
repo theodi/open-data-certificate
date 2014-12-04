bde6fff94eabec8ac93dec27f65eb1393221ad9e
Adam Sven Johnson <adam@pkqk.net>
2014-12-04 12:57:49 +0000

    Precompile admin.js in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ OpenDataCertificate::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( badge.css )
+  config.assets.precompile += %w( badge.css admin.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
