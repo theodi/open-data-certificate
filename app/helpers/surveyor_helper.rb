e37db04fb9241ad56a1f24bdff194f4238a322b6
Adam Sven Johnson <adam@pkqk.net>
2015-01-21 14:33:54 +0000

    Extract cache key to helper method

--- app/helpers/surveyor_helper.rb
+++ app/helpers/surveyor_helper.rb
@@ -11,4 +11,9 @@ module SurveyorHelper
     result = responses.detect{|r| (r.question_id == question.id) && (answer.blank? ? true : r.answer_id == answer.id) && (r.response_group.blank? ? true : r.response_group.to_i == response_group.to_i)}
     result ? result : response_set.responses.build(:question_id => question.id, :response_group => response_group)
   end
+
+  def survey_cache_key
+    # If there were no responses by the user, the cache the blank survey for other new users
+    @responses.empty? ? "blank_survey_#{@response_set.survey_id}_#{@response_set.locale}" : @response_set
+  end
 end
