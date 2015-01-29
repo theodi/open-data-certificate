64f22f7bcfd35be89693d82da3955d311a32297d
Adam Sven Johnson <adam@pkqk.net>
2015-01-29 15:12:32 +0000

    Extract error handling to wrapper method

--- lib/odibot.rb
+++ lib/odibot.rb
@@ -9,14 +9,14 @@ class ODIBot
   end
 
   def response_code
-    code = Rails.cache.fetch(@url) rescue nil
-    if code.nil?
-      code = self.class.get(@url, @options).code
-      Rails.cache.write(@url, code, expires_in: 5.minute)
+    ODIBot.handle_errors(0) do
+      code = Rails.cache.fetch(@url) rescue nil
+      if code.nil?
+        code = self.class.get(@url, @options).code
+        Rails.cache.write(@url, code, expires_in: 5.minute)
+      end
+      code
     end
-    code
-  rescue SocketError, Errno::ETIMEDOUT, Errno::ECONNREFUSED, Errno::ECONNRESET, Errno::EHOSTUNREACH, OpenSSL::SSL::SSLError, Timeout::Error
-    0
   end
 
   def is_http_url?
@@ -33,5 +33,11 @@ class ODIBot
 
   def valid?
     is_http_url? && response_code == 200
+  end
+
+  def self.handle_errors(return_value)
+    return yield
+  rescue SocketError, Errno::ETIMEDOUT, Errno::ECONNREFUSED, Errno::ECONNRESET, Errno::EHOSTUNREACH, OpenSSL::SSL::SSLError, Timeout::Error
+    return return_value
   end
 end
