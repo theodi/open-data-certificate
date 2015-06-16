16fb6256f7b74a2de0baba3388e87a20be9a8ad2
pezholio <pezholio@gmail.com>
2015-06-16 16:53:24 +0100

    This is how we work together with delayed_job

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,1 +1,26 @@
-Sidekiq.hook_rails!
+module Sidekiq::Extensions::Klass
+  alias :sidekiq_delay :delay
+  remove_method :delay
+  alias :sidekiq_delay_for :delay_for
+  remove_method :delay_for
+  alias :sidekiq_delay_until :delay_until
+  remove_method :delay_until
+end
+
+module Sidekiq::Extensions::ActiveRecord
+  alias :sidekiq_delay :delay
+  remove_method :delay
+  alias :sidekiq_delay_for :delay_for
+  remove_method :delay_for
+  alias :sidekiq_delay_until :delay_until
+  remove_method :delay_until
+end
+
+module Sidekiq::Extensions::ActionMailer
+  alias :sidekiq_delay :delay
+  remove_method :delay
+  alias :sidekiq_delay_for :delay_for
+  remove_method :delay_for
+  alias :sidekiq_delay_until :delay_until
+  remove_method :delay_until
+end
