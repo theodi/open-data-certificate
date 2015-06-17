af8873250a936dab209c93a38bf74e3a1bb706ab
pezholio <pezholio@gmail.com>
2015-06-17 14:07:54 +0100

    Add missing responses to CSV

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,10 +1,10 @@
 module CampaignsHelper
 
-  def missing_responses(generator)
+  def missing_responses(generator, seperator = "\r\n")
     response_set = generator.certificate.response_set
     response_set.incomplete_triggered_mandatory_questions
                 .map { |q| q.text }
-                .join("<br />")
+                .join(seperator)
   end
 
 end
