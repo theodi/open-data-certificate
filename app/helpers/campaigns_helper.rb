b4e3f8c92a7c07a0805ac40a45771f1ea94f3114
pezholio <pezholio@gmail.com>
2015-06-17 13:58:05 +0100

    Add missing responses to campaign view

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,2 +1,10 @@
 module CampaignsHelper
+
+  def missing_responses(generator)
+    response_set = generator.certificate.response_set
+    response_set.incomplete_triggered_mandatory_questions
+                .map { |q| q.text }
+                .join("<br />")
+  end
+
 end
