fd19097dff22bc7ac9924ffd98e63b4cff0934b7
pezholio <pezholio@gmail.com>
2015-06-17 16:54:05 +0100

    Use cached missing responses

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,10 +1,9 @@
 module CampaignsHelper
 
   def missing_responses(generator, seperator = "\r\n")
-    response_set = generator.certificate.response_set
-    response_set.incomplete_triggered_mandatory_questions
-                .map { |q| q.text }
-                .join(seperator)
+    generator.response_set.missing_responses
+             .split(",")
+             .join(seperator)
   end
 
 end
