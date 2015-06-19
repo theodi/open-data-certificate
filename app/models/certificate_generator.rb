f062df81e62bfa0cddac42a1a8d188941636a627
pezholio <pezholio@gmail.com>
2015-06-19 10:58:32 +0100

    Pass dataset through to generator

--- app/models/certificate_generator.rb
+++ app/models/certificate_generator.rb
@@ -57,8 +57,8 @@ class CertificateGenerator < ActiveRecord::Base
     end
 
     generator = response_set.certificate_generator || self.create(response_set: response_set, user: user)
-    certificate = generator.generate(jurisdiction, false)
     generator.request = request unless request.nil?
+    certificate = generator.generate(jurisdiction, false, dataset)
     response_set = certificate.response_set
 
     errors = response_set.response_errors
