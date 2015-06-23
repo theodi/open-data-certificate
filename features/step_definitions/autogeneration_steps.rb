8dff4b310e054d22059ee3af3b0fe205fc2e23d1
pezholio <pezholio@gmail.com>
2015-06-23 14:28:49 +0100

    Tweak autocompletion steps

--- features/step_definitions/autogeneration_steps.rb
+++ features/step_definitions/autogeneration_steps.rb
@@ -1,5 +1,7 @@
 Given(/^my URL autocompletes via DataKitten$/) do
-  KittenData.any_instance.stubs(:fields).returns(@body[:dataset])
+  KittenData.any_instance.stubs(:fields).returns(@autocomplete || @body[:dataset])
+
+  @autocomplete = @body[:dataset] # Keep this around for the purposes of regeneration
 
   @body[:dataset] = {
     documentationUrl: @documentationURL
@@ -11,7 +13,7 @@ Given(/^the field "(.*?)" is missing from my metadata$/) do |field|
 end
 
 When(/^I add the field "(.*?)" with the value "(.*?)" to my metadata$/) do |field, value|
-  @body[:dataset][field.to_sym] = value
+  @autocomplete[field.to_sym] = value
 end
 
 When(/^I change the underlying dataset$/) do
