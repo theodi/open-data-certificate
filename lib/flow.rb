572f9e6464b4252fe2655758c26324efa009bc7b
pezholio <pezholio@gmail.com>
2015-07-21 11:41:02 +0100

    Extract XML into Ruby

---
+++ lib/flow.rb
@@ -0,0 +1,85 @@
+class Flow
+
+  def initialize(jurisdiction)
+    jurisdiction = "en" if jurisdiction == "gb"
+    path = File.join(Rails.root, 'prototype', 'translations', "certificate.#{jurisdiction}.xml")
+    @xml = Nokogiri::XML(File.open(path))
+    @xml_copy = Nokogiri::XML(File.open(path))
+  end
+
+  def questions
+    @xml_copy.xpath("//if").remove # remove any dependencies
+    @xml_copy.xpath("//question").map do |q|
+      question(q)
+    end
+  end
+
+  def dependencies
+    @xml.xpath("//if//question").map do |q|
+      question(q, true)
+    end
+  end
+
+  def question(q, dependency = true)
+    {
+      id: q["id"],
+      label: label(q),
+      type: type(q),
+      level: level(q),
+      answers: answers(q),
+      prerequisites: dependency === true ? prerequisites(q) : nil
+    }
+  end
+
+  def answers(question)
+    if type(question) == "yesno"
+      {
+        "true" => {
+          dependency: dependency(question, "true")
+        },
+        "false" => {
+          dependency: dependency(question, "false")
+        }
+      }
+    elsif question.at_xpath("radioset|checkboxset")
+      answers = {}
+      question.at_xpath("radioset|checkboxset").xpath("option").each do |option|
+        answers[option.at_xpath("label").text] = {
+          dependency: dependency(question, option["value"])
+        }
+      end
+      answers
+    end
+  end
+
+  def label(question)
+    question.at_xpath("label").text
+  end
+
+  def type(question)
+    if question.at_xpath("input")
+      "input"
+    elsif question.at_xpath("yesno")
+      "yesno"
+    elsif question.at_xpath("radioset")
+      "radioset"
+    elsif question.at_xpath("checkboxset")
+      "checkboxset"
+    end
+  end
+
+  def level(question)
+    question.at_xpath("requirement")["level"] unless question.at_xpath("requirement").nil?
+  end
+
+  def dependency(question, answer)
+    dependency = @xml.xpath("//if[contains(@test, \"this.#{question["id"]}() === '#{answer}'\")]").at_css("question")
+    dependency["id"] unless dependency.nil?
+  end
+
+  def prerequisites(question)
+    test = question.parent["test"] || question.parent.parent["test"]
+    test.scan(/this\.([a-z]+)\(\)/i).map { |s| s[0] } unless test.nil?
+  end
+
+end
