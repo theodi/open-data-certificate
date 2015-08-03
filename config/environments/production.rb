5ab28c3a655b33828d65fe6060178b13cd82141b
Adam Sven Johnson <adam@pkqk.net>
2015-08-03 14:22:50 +0100

    Include `campaigns.css` from `application.css`
    Instead of adding it to the asset pipeline paths.
    
    It could also possibly be extracted out to an
    admin/backend only css file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ OpenDataCertificate::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( badge.css admin.js campaigns.css)
+  config.assets.precompile += %w( badge.css admin.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
