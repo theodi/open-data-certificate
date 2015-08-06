127779b9e3331833dea17cf0144afd9b26013c32
bcouston <ben.couston@theodi.org>
2015-08-06 15:25:15 +0100

    Correct file path for UK survey
    Back to .UK from .GB

--- features/support/helper.rb
+++ features/support/helper.rb
@@ -34,7 +34,7 @@ Before("@api") do
 end
 
 Before("@survey") do
-  builder = SurveyBuilder.new 'surveys', 'odc_questionnaire.GB.rb'
+  builder = SurveyBuilder.new 'surveys/development', 'odc_questionnaire.UK.rb'
   builder.parse_file
 end
 
