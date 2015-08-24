b4fcc0d03397a596ab2ccb8acf99677c3de0f7f3
Adam Sven Johnson <adam@pkqk.net>
2015-08-24 15:54:57 +0100

    Add sidekiq workers to `eager_load_path`
    sidekiq needs to be told about other code path
    locations as it needs to load everything before
    creating any threads.

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,10 @@ module OpenDataCertificate
       #{config.root}/app/models/concerns
     )
 
+    config.eager_load_paths += %W(
+      #{config.root}/app/workers
+    )
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
