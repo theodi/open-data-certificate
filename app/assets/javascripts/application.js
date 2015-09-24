04adeed4e605b540ea4c48428c4a3ac20391a5de
Tony Marklove <tony@tonymarklove.net>
2015-09-24 14:29:25 +0100

    Update hardcoded English text inside javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -257,6 +257,7 @@ $(document).ready(function($){
   },5000);
 
   $('.survey-intro .submit').click(function() {
+    var button = $(this);
     $(this).addClass('disabled');
     $(this).removeClass('error');
     $(this).popover('show');
@@ -270,14 +271,14 @@ $(document).ready(function($){
         window.location.replace(data.survey_path)
       },
       error: function(xhr) {
-        $('.survey-intro .submit').removeClass('disabled')
-        $('.survey-intro .submit').addClass('error')
-        var popover = $('.survey-intro .submit').data('popover')
+        button.removeClass('disabled')
+        button.addClass('error')
+        var popover = button.data('popover')
         if (xhr.status == 404) {
-          popover.options.content = "There was a problem with your URL, please check, or fill out the explanation"
+          popover.options.content = button.data('content')
           $("#start_url_explanation").attr('style', 'display: block')
         } else {
-          popover.options.content = "Sorry, an error occurred. Please try again."
+          popover.options.content = button.data('error')
         }
         popover.show()
         setTimeout(function(){ popover.hide() }, 3000);
