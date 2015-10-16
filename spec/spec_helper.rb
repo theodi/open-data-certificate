dc3c4af14f1dd26e2ab91b9db085212e65fc76a9
Adam Sven Johnson <adam@pkqk.net>
2015-10-16 13:00:09 +0100

    Include specs in code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-begin
-  require File.expand_path("../../config/environment", __FILE__)
-rescue LoadError => e
-  fail "Could not load the testbed app. Have you generated it?\n#{e.class}: #{e}"
+if ENV['COVERAGE']
+  require 'coveralls'
+  Coveralls.wear_merged! 'rails'
 end
+
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'json_spec'
 require 'database_cleaner'
@@ -24,16 +24,7 @@ ActiveRecord::Base.mass_assignment_sanitizer = :strict
 
 RSpec.configure do |config|
   config.include JsonSpec::Helpers
-
   config.treat_symbols_as_metadata_keys_with_true_values = true
-
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
   config.mock_with :rspec
 
   ## Database Cleaner
