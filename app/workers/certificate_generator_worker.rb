2e917cc0f923509c73466c13710266d76ddc07f4
Adam Sven Johnson <adam@pkqk.net>
2015-10-26 13:29:54 +0000

    Finish job early if generator has already run
    If the generator has already run (sidekiq can sometimes attempt to start
    a job more than once*) then don’t try again as trying to publish an
    already published `ResponseSet` will fail due to state machine rules.
    
    
    * due to CAP

--- app/workers/certificate_generator_worker.rb
+++ app/workers/certificate_generator_worker.rb
@@ -4,7 +4,9 @@ class CertificateGeneratorWorker
   def perform(certificate_generator_id, jurisdiction, create_user, dataset_id)
     generator = CertificateGenerator.find(certificate_generator_id)
     dataset = Dataset.find_by_id dataset_id
-    generator.generate(jurisdiction, create_user, dataset)
+    unless generator.completed?
+      generator.generate(jurisdiction, create_user, dataset)
+    end
   end
 
 end
