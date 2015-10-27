69c0ca79b0d8fca99d028a4eb9b546132d68647b
Tony Marklove <tony@tonymarklove.net>
2015-10-27 14:48:08 +0000

    Add blank (/bl/) locale for finding untranslated strings

--- config/initializers/i18n_custom_backend.rb
+++ config/initializers/i18n_custom_backend.rb
@@ -1,16 +1,29 @@
 module OdiI18nBackend
   def store_translations(locale, data, options={})
     super(locale, data, options)
-    super('db', deep_duplicate_locale_strings(data), options) if locale == 'en'
+    super('db', duplicate_locale_strings(data), options) if locale == 'en'
+    super('bl', underscore_locale_strings(data), options) if locale == 'en'
   end
 
-  def deep_duplicate_locale_strings(data)
+  def duplicate_locale_strings(data)
+    deep_transform_locale_strings(data) do |locale_string|
+      "#{locale_string} #{locale_string}"
+    end
+  end
+
+  def underscore_locale_strings(data)
+    deep_transform_locale_strings(data) do |locale_string|
+      locale_string.gsub(/[^\s]/, '_')
+    end
+  end
+
+  def deep_transform_locale_strings(data, &block)
     data.inject({}) do |result, (k,v)|
       unless v.blank?
         result[k] = if v.is_a?(Hash)
-          deep_duplicate_locale_strings(v)
+          deep_transform_locale_strings(v, &block)
         elsif v.is_a?(String)
-          "#{v} #{v}"
+          block.call(v)
         else
           v
         end
