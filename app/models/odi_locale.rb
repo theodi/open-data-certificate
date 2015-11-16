a20a07fdd876df18416d5006fe62c9ee89a6a4b1
Adam Sven Johnson <adam@pkqk.net>
2015-11-16 17:28:16 +0000

    Handle more complex locales in switching helper

--- app/models/odi_locale.rb
+++ app/models/odi_locale.rb
@@ -1,7 +1,7 @@
 class OdiLocale
 
   def self.changed_locale_path(current_path, new_locale)
-    current_path.gsub(%r{\A/[a-z]{2}/(.*)}, "/#{new_locale}/\\1")
+    current_path.gsub(%r{\A/[a-z]{2}(_[A-Z]{2})?/(.*)}, "/#{new_locale}/\\2")
   end
 
 end
