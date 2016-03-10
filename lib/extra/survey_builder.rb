b4a8f3a5403e8e301254b4e14541b0704ba96720
Adam Sven Johnson <adam@pkqk.net>
2016-03-10 17:08:16 +0000

    Add ability to force a rebuild of a survey

--- lib/extra/survey_builder.rb
+++ lib/extra/survey_builder.rb
@@ -6,10 +6,11 @@ class SurveyBuilder < Struct.new(:dir, :basename)
     survey_parsing.md5 = Digest::MD5.hexdigest(file_contents)
 
     changed = survey_parsing.changed? && survey_parsing.save
+    rebuild = changed || ENV['FORCE_REBUILD'].present?
 
-    record_event "SurveyBuilder: #{dir}/#{basename} - #{changed ? 'building' : 'skipping '} - #{survey_parsing.md5}"
+    record_event "SurveyBuilder: #{dir}/#{basename} - #{rebuild ? 'building' : 'skipping '} - #{survey_parsing.md5}"
 
-    if changed
+    if rebuild
       survey = parse_file
       survey.set_expired_certificates
     end
