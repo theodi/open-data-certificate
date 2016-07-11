a07d038a3d6e42283410115ed2a546857f4bdeb9
Rory Gianni <giannichan@gmail.com>
2016-07-11 18:46:17 +0100

    corrects method call

--- app/workers/certificate_generator_update_worker.rb
+++ app/workers/certificate_generator_update_worker.rb
@@ -4,7 +4,7 @@ class CertificateGeneratorUpdateWorker
   def perform(certificate_generator_id, jurisdiction, user_id)
     generator = CertificateGenerator.find(certificate_generator_id)
     create_user = User.find(user_id)
-    generator.update(generator.response_set_id, nil, jurisdiction, create_user)
+    CertificateGenerator.update(generator.dataset, nil, jurisdiction, create_user)
   end
 
 end
