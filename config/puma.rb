60f9548e8f3d18cd3dd3eb05d4d6d068c0603232
James Smith <james@floppy.org.uk>
2017-11-06 23:51:51 +0000

    worker config for Rails <4.1

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,12 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+  # Valid on Rails up to 4.1 the initializer method of setting `pool` size
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+             Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['RAILS_MAX_THREADS'] || 5
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
